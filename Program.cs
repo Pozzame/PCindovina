82f44bc5dcbe4e1caf8160b66a575deab2370683
Pozzame <pozzame@gmail.com>
2024-07-03 13:03:03 +0200

    Ok

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Range
+int min = 0;
+int max = 100;
+//
+
+Console.WriteLine($"Immagina un numero da {min} a {max}\n"); //Intro
+
+//Inizializzazione
+char inserimento = 'o';
+int num = TrovatoreBinario(min,max);
+//
+
+while(inserimento != 's') //Esce con 's'
+{
+    Console.Clear();
+    Console.WriteLine($"Forse è: {num}?\ns - Sì\na - Prova più alto\nb - Prova più basso");
+    inserimento = Console.ReadKey(true).KeyChar;
+    if (inserimento == 'a')
+    {
+        min = num;
+        num = TrovatoreBinario(min,max);
+    } else if (inserimento == 'b')
+    {
+        max = num;
+        num = TrovatoreBinario(min,max);
+    }else if (inserimento != 's') Console.WriteLine("Non ho capito. Riprova");
+}
+Console.WriteLine(":-D");
+
+int TrovatoreBinario(int min, int max)
+{
+   return ((max-min)/2)+min;
+}
