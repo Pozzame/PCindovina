e352b1ab8ef3ace7860b026cd279a13e9900a9a8
Pozzame <pozzame@gmail.com>
2024-07-03 13:55:39 +0200

    Ok

--- Program.cs
+++ Program.cs
@@ -7,27 +7,17 @@ Console.WriteLine($"Immagina un numero da {min} a {max}\n"); //Intro
 
 //Inizializzazione
 char inserimento = 'o';
-int num = TrovatoreBinario(min,max);
+int num = 50;
 //
 
 while(inserimento != 's') //Esce con 's'
 {
-    Console.Clear();
     Console.WriteLine($"Forse è: {num}?\ns - Sì\na - Prova più alto\nb - Prova più basso");
     inserimento = Console.ReadKey(true).KeyChar;
-    if (inserimento == 'a')
-    {
-        min = num;
-        num = TrovatoreBinario(min,max);
-    } else if (inserimento == 'b')
-    {
-        max = num;
-        num = TrovatoreBinario(min,max);
-    }else if (inserimento != 's') Console.WriteLine("Non ho capito. Riprova");
+    Console.Clear();
+    if (inserimento == 'a') min = num;
+    else if (inserimento == 'b') max = num;
+    else if (inserimento != 's') continue;
+    num = ((max-min)/2)+min;
 }
 Console.WriteLine(":-D");
-
-int TrovatoreBinario(int min, int max)
-{
-   return ((max-min)/2)+min;
-}
