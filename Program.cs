7c6f10ac7ac28180bb6ef15c91ecb64cdc2e83f6
Pozzame <pozzame@gmail.com>
2024-07-05 12:17:20 +0200

    Ok

--- Program.cs
+++ Program.cs
@@ -1,28 +1,30 @@
-﻿//Range
+﻿//Inizializzazione
 int min = 0;
 int max = 100;
+int maxTentativi = 10;
+Random numeroCasuale = new Random();
+char inserimento = 'o';
+int tentativi;
 //
 
 Console.WriteLine($"Immagina un numero da {min} a {max}\n"); //Intro
 
-//Inizializzazione
-Random rng = new Random();
-char inserimento = 'o';
-int num = rng.Next(max+1); //primo tentativo
-int tentativi;
-//
+int num = numeroCasuale.Next(min, max + 1); //Primo tentativo
 
-    for (tentativi = 1; tentativi <= 5; tentativi++)
-    {
-        Console.Clear();
-        Console.WriteLine("Tentativo: " + tentativi);
-        Console.WriteLine($"Forse è: {num}?\nc - Sì\n+ - Prova più alto\n- - Prova più basso");
-        inserimento = Console.ReadKey(true).KeyChar;
-        if (inserimento == '+') min = num++; else if (inserimento == '-') max=num--; else if (inserimento != 'c') { tentativi--; continue;} else if (inserimento == 'c') break;
-        num = rng.Next(min,max+1);//num = ((max - min) / 2) + min;
-    }
+for (tentativi = 1; tentativi <= maxTentativi; tentativi++)
+{
+    Console.Clear();
+    Console.WriteLine($"Tentativo: {tentativi}/{maxTentativi} Tra: {min} e {max}\nForse è: {num}?\nc - Sì\n+ - Prova più alto\n- - Prova più basso");
+    inserimento = Console.ReadKey(true).KeyChar;
+    if (inserimento == '+') min = num++;
+    else if (inserimento == '-') max = num--;
+    else if (inserimento != 'c') { tentativi--; continue; }
+    else if (inserimento == 'c') break;
 
-if (inserimento=='c') Console.WriteLine("Ho vinto!"); else {Console.WriteLine("Ho perso."); tentativi--;}
+    num = numeroCasuale.Next(min, max + 1);
+}
+
+if (inserimento == 'c') Console.WriteLine("Ho vinto!"); else { Console.WriteLine("Ho perso."); tentativi--; }
 Console.WriteLine($"Tentativi effettuati: {tentativi}");
 
 
@@ -30,12 +32,12 @@ Console.WriteLine($"Tentativi effettuati: {tentativi}");
 
 /*
 int[] dadi = new int[5];
-Random rng = new Random();
+Random numeroCasuale = new Random();
 
 //Lancio i 5 dadi
 for (int i=0;i<dadi.Length;i++) 
 {
-    dadi[i]=rng.Next(1, 7);
+    dadi[i]=numeroCasuale.Next(1, 7);
     Console.WriteLine($"{i+1} = {dadi[i]}");
 }
 //
@@ -44,5 +46,5 @@ Console.WriteLine("Quali dadi vuoi cambiare?");
 string? tenere = Console.ReadLine(); //Leggo una stringa che contiene i dadi da ri-lanciare
 List<int> tenuti = new List<int>();
 for (int i=0;i<tenere!.Length;i++) tenuti.Add(Convert.ToInt32(tenere.Substring(i, 1))); //Converto la stringa in una lista contenente i dadi da ri-lanciare
-for (int i=0;i<tenuti.Count;i++) dadi[tenuti[i]-1]=rng.Next(1, 7); //Ri-lancio solo i dadi da ri-lanciare
+for (int i=0;i<tenuti.Count;i++) dadi[tenuti[i]-1]=numeroCasuale.Next(1, 7); //Ri-lancio solo i dadi da ri-lanciare
 for (int i=0;i<dadi.Length;i++) Console.WriteLine($"{i+1} = {dadi[i]}"); //Ri-scrivo tutti i dadi*/
